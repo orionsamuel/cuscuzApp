0344538404657087c6f9c42ee7df0828d9a91adc
Samuel Oliveira <s.oliveira.15@hotmail.com>
2022-06-24 02:24:18 -0300

    Criando tela de cadastro de usuário e adicionando toogle button na lista de participantes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -15,6 +15,8 @@ export class Tab1Page {
   titulo = 'Inscritos Cuscuz HQ';
 
   listaInscritos: IInscritos;
+  presente: boolean;
+  mensagem: string;
 
   constructor(
     public alertController: AlertController,
@@ -33,5 +35,45 @@ export class Tab1Page {
           this.listaInscritos = dados;
         });
       }
+    }
+
+    async exibirAlertaPresenca(event: boolean) {
+      this.presente = event;
+      if(this.presente){
+        this.mensagem = 'Deseja colocar participante como ausente?';
+      }else{
+        this.mensagem = 'Deseja colocar participante como presente?';
+      }
+      const alert = await this.alertController.create({
+        header: 'Alerta',
+        message: this.mensagem,
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            id: 'cancel-button',
+            handler: (blah) => {
+              console.log('Confirm Cancel: blah');
+            }
+          }, {
+            text: 'SIM, Alterar Presença',
+            id: 'confirm-button',
+            handler: () => {
+              this.apresentarToast();
+            }
+          }
+        ]
+      });
+
+      await alert.present();
+    }
+
+    async apresentarToast() {
+      const toast = await this.toastController.create({
+        message: 'Alterada a presença do participante.',
+        duration: 2000,
+        color: 'success '
+      });
+      toast.present();
     }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { FormGroup, FormControl } from '@angular/forms';
+import { InscritosService } from './../services/inscritos.service';
+import { IInscritos } from './../models/IInscritos.model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  titulo = 'Cadastrar Participante';
+
+  nome: string;
+  telefone: string;
+  email: string;
+
+  constructor(public toastController: ToastController) {}
+
+  ngOnInit() {
+  }
+
+  cadastro(){
+    this.presentToast('Cadastrado Com Sucesso', 'success');
+  }
+
+  async presentToast(texto: string, cor: string) {
+    const toast = await this.toastController.create({
+      message: texto,
+      color: cor,
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
