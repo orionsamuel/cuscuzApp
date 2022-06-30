0ece314b962de54df4d827b7fbdd1c8c593edf2c
Samuel Oliveira <s.oliveira.15@hotmail.com>
2022-06-30 11:24:18 -0300

    Adicionando a visualização dos participantes do cospobre

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { CospobreService } from '../services/cospobre.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,18 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  listaParticipantes: any[];
 
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController,
+    public route: Router,
+    public cospobreService: CospobreService) {this.buscarParticipante();}
+
+    buscarParticipante(){
+      this.cospobreService.buscarParticipante().subscribe(dados=>{
+        console.log(dados);
+        this.listaParticipantes = dados;
+      });
+    }
 }
